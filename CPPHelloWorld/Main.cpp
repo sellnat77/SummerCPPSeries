c258f0f3a2c0cbc62ed95dc882c4bf0cf3782da3
sellnat77 <sell_nat@yahoo.com>
2014-07-08 22:26:58 -0700

    video 2

--- CPPHelloWorld/Main.cpp
+++ CPPHelloWorld/Main.cpp
@@ -4,9 +4,45 @@ using namespace std;
 
 int main()
 {
-	//cout << "Hello World!\n\n\n\n\n\n"; after video #1
+	/*cout << "Hello World!\n\n\n\n\n\n"; after video #1
+	int apples;
+	int banana;
+	string Name;
+
+	apples = 98;
+	banana = 7;
+
+	Name = "Russell";
+
+	cout << "you start with " << apples;
+	cout << "\nTake away the amount of bananas that you have and you are left with...";
+	cout << "\n\t " << apples-banana << "\n\n\n\n";
+
+	cout << "If you add the bananas and apples together you get: ";
+
+	apples += banana;
+
+	cout << apples << " apples.\n\n\n\n\n";
+	*/
+
+	int apples;
+	int bananas;
+	int total;
+
+	cout << "How many apples do you have? \n";
+	cin >> apples;
+	cout << "\n";
+
+	cout << "How many bananas do you have? \n";
+	cin >> bananas;
+	cout << "\n";
+
+	total = apples + bananas;
+
+	cout << "The total number of apples and bananas you have are: ";
+	cout << total;
+	cout << "\n\n";
 
 
-
-		return 0;
+	return 0;
 }
